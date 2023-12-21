b93a83fa450101e6441f01211c4ba6ce5a66491f
yongyae <kasama191246@gmail.com>
2023-12-21 19:29:38 +0700

    สูตรคูณ

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
-<!DOCTYPE html>
 <html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
 <body>
-    <h1>This is php <?php echo 'Hello world!' ; ?></h1>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            <tr>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
 </body>
+
 </html>
---
+++ workshop2.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="my_val"/>
+        <button type="submit"> บันทึก</button>
+
+    </form>
+</body>
+</html>
